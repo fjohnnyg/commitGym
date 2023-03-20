50a535d7ee573ca95140a9b73173d8b1e07762de
joaogilferreira <joaogilferreira@hotmail.com>
2023-03-20 18:16:17 +0000

    Initial commit

---
+++ src/main/java/academy/mindswap/finalproject/CommitGymApplication.java
@@ -0,0 +1,13 @@
+package academy.mindswap.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/mindswap/finalproject/CommitGymApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.mindswap.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
